24c6aac58d94bbb418f4381b4aa2379694381324
josh lehne <jdlehne@gmail.com>
2017-07-20 13:31:07 -0500

    fixed counterAttack and changed style

--- assets/jquery/game.js
+++ assets/jquery/game.js
@@ -4,8 +4,9 @@ $(document).ready(function() { //beginning of char select bracketg
         "john-snow": {
             name: "John Snow",
             hitpoints: 120,
-            attack: 30,
-            image: "assets/css/images.snow.jpg",
+            attack: 18,
+            baseAttack: 18,
+            image: "assets/css/images.snow1.jpg",
             counterAttack: 45,
         },
 
@@ -13,7 +14,8 @@ $(document).ready(function() { //beginning of char select bracketg
             name: "Daenyreus",
             hitpoints: 160,
             attack: 7,
-            image: "assets/css/images/daen.jpg",
+            baseAttack: 7,
+            image: "assets/css/images/daen1.jpg",
             counterAttack: 15,
         },
 
@@ -21,7 +23,8 @@ $(document).ready(function() { //beginning of char select bracketg
             name: "Cersei Lannister",
             hitpoints: 100,
             attack: 20,
-            image: "assets/css/images/cersei.jpg",
+            baseAttack: 20,
+            image: "assets/css/images/cersei1.jpg",
             counterAttack: 25,
         },
 
@@ -29,16 +32,18 @@ $(document).ready(function() { //beginning of char select bracketg
             name: "The Night King",
             hitpoints: 130,
             attack: 25,
-            image: "assets/css/images/nightking.jpg",
+            baseAttack: 25,
+            image: "assets/css/images/nightking1.jpg",
             counterAttack: 25,
         },
 
     }; //end char block
-
+    var wins = 0;
     var playerSelected = false;
     var enemySelected = false;
     var death = new Audio("assets/death.mp3");
-    
+    var fight = new Audio("assets/fight.mp3");
+
     // write health into footer slot for each character from their Object block
     $("#john-snow").find("#health").html(characters["john-snow"].hitpoints);
     $("#daenyreus").find("#health").html(characters["daenyreus"].hitpoints);
@@ -60,16 +65,17 @@ $(document).ready(function() { //beginning of char select bracketg
                 $("#oppenentChoice").children().addClass("enemy");
                 $("#choose").html("defend your stake to the throne");
                 $("#phase").html("Attack your oppenent");
-                $("#characterChoice").hide("slow");//-----------------Bug hide other chars
-                enemySelected = true; 
+                $("#characterChoice").hide("slow"); 
+                enemySelected = true;
             } //drops enemy to oppenent section
 
 
             //Once both characters are set, use for attack
-            $("#attackBtn").unbind('click').click(function(evt) {  
-                $("#characterChoice").hide("slow");   
+            $("#attackBtn").unbind('click').click(function(evt) {
+                $("#characterChoice").hide("slow");
+                fight.play();
                 //---------create variable for player attack increase---------
-                var attackIncrease = characters[$(".playerChoice").attr('id')].attack;  
+                var attackIncrease = characters[$(".playerChoice").attr('id')].baseAttack;
                 //--------damage player takes on turn-------------
                 characters[$(".playerChoice").attr('id')].hitpoints -= characters[$(".enemy").attr('id')].counterAttack;
                 console.log(characters[$(".playerChoice").attr('id')].name + " " + characters[$(".playerChoice").attr('id')].hitpoints);
@@ -82,36 +88,49 @@ $(document).ready(function() { //beginning of char select bracketg
                 $("#results").html("the enemy attacked you for " + characters[$(".enemy").attr('id')].counterAttack + " and " + ("you attacked the enemy for  " + characters[$(".playerChoice").attr('id')].attack));
 
 
-                //-------------FIX attack multiplier---------------------------------------------Bugged-------------!!!
+                //-------------Attack increases on each attack by base amount------//
                 characters[$(".playerChoice").attr('id')].attack += attackIncrease;
                 console.log("The player's attack is now  " + characters[$(".playerChoice").attr('id')].attack);
 
 
                 //----------Conditions for game Wins---------------//
 
-                  //If Game over, states player is dead and attack btn becomes try agaiain, reload on location.reload click
-                if (characters[$(".playerChoice").attr('id')].hitpoints <= 0) {
+                //If Game over, states player is dead and attack btn becomes try agaiain, reload on location.reload click
+
+                if (wins === 3) {
+                    console.log("player wins");
+                    $("#results").html("you win");
+                    $("#attackBtn").html("play again");
+                
+                }  
+
+                else if (characters[$(".playerChoice").attr('id')].hitpoints <= 0) {
+                    $("#body").css('background-image','url(assets/css/images/background.jpg center fixed');
                     $("#results").html(characters[$(".playerChoice").attr('id')].name + " is dead try again?");
                     $("#attackBtn").html("Try Again");
                     $("#attackBtn").unbind("click");
                     $("#attackBtn").click(function() {
                         location.reload();
-                      
+
 
                     });
 
-                } else if (characters[$(".enemy").attr('id')].hitpoints <= 0) {
-                            death.play();
-                            $("#characterChoice").show("slow");
-                             $("#results").html("You Defeated  " + characters[$(".enemy").attr('id')].name + " please  Choose  another  contender  to  the  iron  throne");
-                             $("#phase").html("Select a new oppenent");
-                             enemySelected = false;
-                             pickChar();
+                }
 
-                         } else if(characters[$(".enemy").attr('id')].hitpoints <= 0 && enemySelected === 3) {
-                                     $("#results").html("you win");
-                                    }//----------WIN VARIABLE NOT SET------------------//
-              
+                else if (characters[$(".enemy").attr('id')].hitpoints <= 0) {
+                    death.play();
+                    wins++;
+                    console.log("wins: " + wins);
+                    $("#characterChoice").show("slow");
+                    $("#results").html("You Defeated  " + characters[$(".enemy").attr('id')].name + " please  Choose  another  contender  to  the  iron  throne");
+                    $("#phase").html("Select a new oppenent");
+                    enemySelected = false;
+                    pickChar();
+
+                }
+
+               //----------WIN VARIABLE NOT SET------------------//
+
 
             }); //attack function
 
